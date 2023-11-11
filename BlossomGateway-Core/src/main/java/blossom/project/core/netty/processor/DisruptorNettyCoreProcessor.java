4b80c1295620bbf316ab8ef00cd56b78f3a7fcbf
jinbiao.zhang <460219753@qq.com>
2023-11-11 16:21:55 +0800

    注释完整化

--- BlossomGateway-Core/src/main/java/blossom/project/core/netty/processor/DisruptorNettyCoreProcessor.java
+++ BlossomGateway-Core/src/main/java/blossom/project/core/netty/processor/DisruptorNettyCoreProcessor.java
@@ -20,90 +20,112 @@ import lombok.extern.slf4j.Slf4j;
  * @github: https://github.com/ZhangBlossom
  *
  */
+
+/**
+ * DisruptorNettyCoreProcessor 使用 Disruptor 提高性能的 Netty 处理器。
+ * 这个处理器是一个缓存层，通过 Disruptor 来异步处理 HTTP 请求，减轻 Netty 核心处理器的负担。
+ */
 @Slf4j
 public class DisruptorNettyCoreProcessor implements NettyProcessor {
+
     /**
      * 线程前缀
      */
-    private static  final String THREAD_NAME_PREFIX = "gateway-queue-";
+    private static final String THREAD_NAME_PREFIX = "gateway-queue-";
 
     private Config config;
 
     /**
-     * Disruptor只是缓存依旧需要用到Netty核心处理器
+     * Disruptor 只是缓存依然需要使用到 Netty 核心处理器
      */
-
     private NettyCoreProcessor nettyCoreProcessor;
+
     /**
      * 处理类
      */
     private ParallelQueueHandler<HttpRequestWrapper> parallelQueueHandler;
 
+    /**
+     * 构造方法，初始化 DisruptorNettyCoreProcessor。
+     *
+     * @param config             配置信息对象。
+     * @param nettyCoreProcessor Netty 核心处理器。
+     */
     public DisruptorNettyCoreProcessor(Config config, NettyCoreProcessor nettyCoreProcessor) {
         this.config = config;
         this.nettyCoreProcessor = nettyCoreProcessor;
+
+        // 使用 Disruptor 创建并配置处理队列。
         ParallelQueueHandler.Builder<HttpRequestWrapper> builder = new ParallelQueueHandler.Builder<HttpRequestWrapper>()
                 .setBufferSize(config.getBufferSize())
                 .setThreads(config.getProcessThread())
                 .setProducerType(ProducerType.MULTI)
                 .setNamePrefix(THREAD_NAME_PREFIX)
                 .setWaitStrategy(config.getWaitStrategy());
-        //监听事件处理类
+
+        // 监听事件处理类
         BatchEventListenerProcessor batchEventListenerProcessor = new BatchEventListenerProcessor();
         builder.setListener(batchEventListenerProcessor);
         this.parallelQueueHandler = builder.build();
-
     }
 
+    /**
+     * 处理 HTTP 请求，将请求添加到 Disruptor 处理队列中。
+     *
+     * @param wrapper HttpRequestWrapper 包装类。
+     */
     @Override
     public void process(HttpRequestWrapper wrapper) {
         this.parallelQueueHandler.add(wrapper);
     }
 
-
     /**
-     * 监听处理类
+     * 监听处理类，处理从 Disruptor 处理队列中取出的事件。
      */
     public class BatchEventListenerProcessor implements EventListener<HttpRequestWrapper> {
 
         @Override
         public void onEvent(HttpRequestWrapper event) {
+            // 使用 Netty 核心处理器处理事件。
             nettyCoreProcessor.process(event);
-
         }
 
         @Override
         public void onException(Throwable ex, long sequence, HttpRequestWrapper event) {
             HttpRequest request = event.getRequest();
             ChannelHandlerContext ctx = event.getCtx();
-            try {
-                log.error("BatchEventListenerProcessor onException请求写回失败，request:{},errMsg:{} ",request,ex.getMessage(),ex);
 
-                //构建响应对象
+            try {
+                log.error("BatchEventListenerProcessor onException 请求写回失败，request:{}, errMsg:{} ", request, ex.getMessage(), ex);
+
+                // 构建响应对象
                 FullHttpResponse fullHttpResponse = ResponseHelper.getHttpResponse(ResponseCode.INTERNAL_ERROR);
-                if(!HttpUtil.isKeepAlive(request)){
+
+                if (!HttpUtil.isKeepAlive(request)) {
                     ctx.writeAndFlush(fullHttpResponse).addListener(ChannelFutureListener.CLOSE);
-                }else{
+                } else {
                     fullHttpResponse.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
                     ctx.writeAndFlush(fullHttpResponse);
                 }
-            }catch (Exception e){
-                log.error("BatchEventListenerProcessor onException请求写回失败，request:{},errMsg:{} ",request,e.getMessage(),e);
+            } catch (Exception e) {
+                log.error("BatchEventListenerProcessor onException 请求写回失败，request:{}, errMsg:{} ", request, e.getMessage(), e);
             }
         }
     }
 
-
+    /**
+     * 启动 DisruptorNettyCoreProcessor，启动处理队列。
+     */
     @Override
     public void start() {
         parallelQueueHandler.start();
     }
 
+    /**
+     * 关闭 DisruptorNettyCoreProcessor，关闭处理队列。
+     */
     @Override
     public void shutDown() {
         parallelQueueHandler.shutDown();
     }
-
-
-
 }
--- BlossomGateway-Core/src/main/java/blossom/project/core/netty/processor/NettyCoreProcessor.java
+++ BlossomGateway-Core/src/main/java/blossom/project/core/netty/processor/NettyCoreProcessor.java
@@ -35,44 +35,71 @@ import java.util.concurrent.TimeoutException;
  * @blog: https://blog.csdn.net/Zhangsama1
  * @github: https://github.com/ZhangBlossom
  */
+
+/**
+ * NettyCoreProcessor 是负责在基于 Netty 的服务器中处理 HTTP 请求的组件。
+ */
 @Slf4j
 public class NettyCoreProcessor implements NettyProcessor {
 
+    // FilterFactory 负责创建和管理过滤器的执行。
     private FilterFactory filterFactory = GatewayFilterChainFactory.getInstance();
 
+    /**
+     * 处理传入的 HTTP 请求。
+     *
+     * @param wrapper 包含 FullHttpRequest 和 ChannelHandlerContext 的 HttpRequestWrapper。
+     */
     @Override
     public void process(HttpRequestWrapper wrapper) {
         FullHttpRequest request = wrapper.getRequest();
         ChannelHandlerContext ctx = wrapper.getCtx();
 
         try {
+            // 创建并填充 GatewayContext 以保存有关传入请求的信息。
             GatewayContext gatewayContext = RequestHelper.doContext(request, ctx);
-            //执行过滤器逻辑
+
+            // 在 GatewayContext 上执行过滤器链逻辑。
             filterFactory.buildFilterChain(gatewayContext).doFilter(gatewayContext);
         } catch (BaseException e) {
-            log.error("process error {} {}", e.getCode().getCode(), e.getCode().getMessage());
+            // 通过记录日志并发送适当的 HTTP 响应处理已知异常。
+            log.error("处理错误 {} {}", e.getCode().getCode(), e.getCode().getMessage());
             FullHttpResponse httpResponse = ResponseHelper.getHttpResponse(e.getCode());
             doWriteAndRelease(ctx, request, httpResponse);
         } catch (Throwable t) {
-            log.error("process unkown error", t);
+            // 通过记录日志并发送内部服务器错误响应处理未知异常。
+            log.error("处理未知错误", t);
             FullHttpResponse httpResponse = ResponseHelper.getHttpResponse(ResponseCode.INTERNAL_ERROR);
             doWriteAndRelease(ctx, request, httpResponse);
         }
-
     }
 
+    /**
+     * 将 HTTP 响应写入通道并释放资源。
+     *
+     * @param ctx           用于写入响应的 ChannelHandlerContext。
+     * @param request       从客户端接收的 FullHttpRequest。
+     * @param httpResponse 作为响应发送的 FullHttpResponse。
+     */
     private void doWriteAndRelease(ChannelHandlerContext ctx, FullHttpRequest request, FullHttpResponse httpResponse) {
         ctx.writeAndFlush(httpResponse)
-                .addListener(ChannelFutureListener.CLOSE); //释放资源后关闭channel
-        ReferenceCountUtil.release(request);
+                .addListener(ChannelFutureListener.CLOSE); // 发送响应后关闭通道。
+        ReferenceCountUtil.release(request); // 释放与请求相关联的资源。
     }
+
+    /**
+     * 启动 NettyCoreProcessor。（目前为空方法）
+     */
     @Override
     public void start() {
-
+        // 如果需要，启动处理器的实现。
     }
 
+    /**
+     * 关闭 NettyCoreProcessor。（目前为空方法）
+     */
     @Override
     public void shutDown() {
-
+        // 如果需要，关闭处理器的实现。
     }
 }
