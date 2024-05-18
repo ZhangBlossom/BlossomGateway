6d1587a51f270aa09ca72fef8dc459a0a2929914
ZhangBlossom <460219753@qq.com>
2024-05-19 00:11:29 +0800

    准备编写权重负载均衡过滤器

---
+++ BlossomGateway-Core/src/main/java/blossom/project/core/filter/loadbalance/WeightLoadBalanceRule.java
@@ -0,0 +1,72 @@
+package blossom.project.core.filter.loadbalance;
+
+import blossom.project.common.config.DynamicConfigManager;
+import blossom.project.common.config.ServiceInstance;
+import blossom.project.common.exception.NotFoundException;
+import blossom.project.core.context.GatewayContext;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static blossom.project.common.enums.ResponseCode.SERVICE_INSTANCE_NOT_FOUND;
+
+/**
+ * @author: ZhangBlossom
+ * @date: 2024/5/18 23:45
+ * @contact: QQ:4602197553
+ * @contact: WX:qczjhczs0114
+ * @blog: https://blog.csdn.net/Zhangsama1
+ * @github: https://github.com/ZhangBlossom
+ * @description:
+ */
+@Slf4j
+public class WeightLoadBalanceRule implements LoadBalanceGatewayRule {
+
+
+    private final String serviceId;
+
+    /**
+     * 服务列表
+     */
+    private Set<ServiceInstance> serviceInstanceSet;
+
+    public WeightLoadBalanceRule(String serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    private static ConcurrentHashMap<String, RandomLoadBalanceRule> serviceMap = new ConcurrentHashMap<>();
+
+    public static RandomLoadBalanceRule getInstance(String serviceId) {
+        RandomLoadBalanceRule loadBalanceRule = serviceMap.get(serviceId);
+        if (loadBalanceRule == null) {
+            loadBalanceRule = new RandomLoadBalanceRule(serviceId);
+            serviceMap.put(serviceId, loadBalanceRule);
+        }
+        return loadBalanceRule;
+    }
+
+
+    @Override
+    public ServiceInstance choose(GatewayContext ctx, boolean gray) {
+        String serviceId = ctx.getUniqueId();
+        return choose(serviceId,gray);
+    }
+
+    @Override
+    public ServiceInstance choose(String serviceId,boolean gray) {
+        Set<ServiceInstance> serviceInstanceSet =
+                DynamicConfigManager.getInstance().getServiceInstanceByUniqueId(serviceId,gray);
+        if (serviceInstanceSet.isEmpty()) {
+            log.warn("No instance available for:{}", serviceId);
+            throw new NotFoundException(SERVICE_INSTANCE_NOT_FOUND);
+        }
+        List<ServiceInstance> instances = new ArrayList<ServiceInstance>(serviceInstanceSet);
+        int index = ThreadLocalRandom.current().nextInt(instances.size());
+        ServiceInstance instance = (ServiceInstance) instances.get(index);
+        return instance;
+    }
+}
